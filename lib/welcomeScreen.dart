f08886d56ea94025ac0bac52c5d1fa7ddc277134
exam <exam@Aptechnk1.edu>
2024-09-26 17:46:25 +0500

    neha

--- lib/welcomeScreen.dart
+++ lib/welcomeScreen.dart
@@ -7,12 +7,25 @@ class Welcomescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black,
+        // color: Colors.black,
         height: 300,
         width: 200,
-        child: const Text(
-          "Neha",
-          style: TextStyle(color: Color.fromARGB(255, 126, 235, 38)),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(15), topRight: Radius.circular(15)),
+        ),
+        child: Center(
+          child: const Text(
+            "Neha",
+            style: TextStyle(
+              color: Color.fromARGB(255, 126, 235, 38),
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 3,
+              height: 7,
+            ),
+          ),
         ),
       ),
     );
